2d43f211af691461632231fbf3e8f32878ca1cab
AlisterV <82620958+AlisterV@users.noreply.github.com>
2021-06-17 12:37:39 -0400

    Add files via upload

--- dprimegraph_alloptions.m
+++ dprimegraph_alloptions.m
@@ -1,14 +1,17 @@
-%% Last edit made by Alister Virkler 6/4/2021
-%This function, has nested functions within. The overal goal of this
-%function is to display, Go Hit, False Alarm, d', and overall percent
-%correct graphs for the files selected. Also, a performance report
-%spreadsheet is made.
+%% Last edit made by Alister Virkler 6/17/2021
+%This code calls two others to determine whether the user wants to separate
+%the data for d prime and % correct graphs by session or by day
 
 function dprimegraph_alloptions()
+%propmts the user if they would like to separate data by day or session
 answer=questdlg('Would you like data plotted by Day or by Session?','Option 1','Day','Session','Day');
 switch answer
+    %if the user clicks 'Day'
     case 'Day'
+        %this function is called and the code procedes there
         filechoice_normalized_days()
+    %if the user clicks 'Session'
     case 'Session'
+        %this function is called and the code procedes there
         filechoice_normalized_sessions()
 end
